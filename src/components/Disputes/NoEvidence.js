dea8c6ae1b1839ee21ef1cc3c9ed63dadcbc38a3
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-10-26 16:49:23 -0300

    rename svg import

--- src/components/Disputes/NoEvidence.js
+++ src/components/Disputes/NoEvidence.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 import MessageCard from '../MessageCard'
-import noEvidenceSvg from '../../assets/noDraft.svg'
+import noDraftSvg from '../../assets/noDraft.svg'
 
 function NoEvidence() {
   const title = ' The evidence is being presented'
   const paragraph =
     'The involved parties have up to 7 days to submit evidence supporting their case'
 
-  return (
-    <MessageCard title={title} paragraph={paragraph} icon={noEvidenceSvg} />
-  )
+  return <MessageCard title={title} paragraph={paragraph} icon={noDraftSvg} />
 }
 
 export default NoEvidence
