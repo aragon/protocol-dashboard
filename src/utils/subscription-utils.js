1459742a99165b5a6c7b15d03d8b31d68a7ffff5
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-11-09 13:26:52 -0300

    Update relevant attributes

--- src/utils/subscription-utils.js
+++ src/utils/subscription-utils.js
@@ -2,16 +2,19 @@ import { toMs } from './date-utils'
 import { bigNum } from '../lib/math-utils'
 
 export function transformSubscriptionModuleDataAttributes(subscriptionModule) {
+  if (!subscriptionModule) {
+    return null
+  }
+
   return {
     ...subscriptionModule,
     currentPeriod: parseInt(subscriptionModule.currentPeriod, 10),
-    feeAmount: bigNum(subscriptionModule.feeAmount),
     periodDuration: toMs(parseInt(subscriptionModule.periodDuration)),
     periods: subscriptionModule.periods.map(period => ({
       ...period,
       id: parseInt(period.id, 10),
       totalActiveBalance: period.totalActiveBalance,
-      collectedFees: bigNum(period.collectedFees),
+      donatedFees: bigNum(period.donatedFees),
     })),
   }
 }
