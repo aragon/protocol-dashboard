d0a66f25227dd7c699b3348eae922cd2bb2bb813
Giorgi-Lagidze <giorgi.lagidze@toptal.com>
2021-04-06 23:35:40 +0400

    update decode input for action's calldata

--- src/utils/abi-utils.js
+++ src/utils/abi-utils.js
@@ -2,64 +2,70 @@ import { BigNumber, utils } from 'ethers';
 
 function decodeValue(value) {
   if(typeof value === 'number' || BigNumber.isBigNumber(value)) return value.toString()
-
-  if(Array.isArray(value)) {
-    value = value.map(item => {
-      if(typeof item === 'number' || BigNumber.isBigNumber(item)) return item.toString()
-
-      return item
-    })
-  }
-
+  if(Array.isArray(value)) 
+    value = value.map(decodeValue)
   return value
 }
 
-function decodeInput(input, value, accum) {
-  if(input.type === 'tuple') {
-    accum[input.name] = {};
-    input.components.forEach((component, index) => {
-      if(component.type === 'tuple') {
-        accum[input.name][component.name] = {};
-        decodeInput(component, value[index], accum[input.name])
+/**
+ * structures the decoded calldata in a concise way. 
+ * 
+ * @param {string} name the name of the element
+ * @param {object} input abi's component
+ * @param {any} value the actual parameter value user called
+ * @param {object} accum the final decoded result
+ * @returns {void}
+ */
+function decodeInput(name, input, value, accum) {
+  if(!accum[name]) accum[name] = {}
+
+  if(input.arrayChildren) {
+    accum[name] = {}; 
+    value.map((item, index) => {
+      let key = `${index+1}th element`
+      if(input.arrayChildren) {
+        key += `(${input.arrayChildren.baseType})`
       }
-      else{
-        accum[input.name][component.name] = decodeValue(value[index])
-      }
+      accum[name][key] = {}
+      decodeInput(key, input.arrayChildren, item, accum[name])
     })
-  }else{
-    accum[input.name] = decodeValue(value)
+    return;
   }
+
+  if(input.type === 'tuple') {
+    input.components.forEach((component, index) => {
+      decodeInput(`${component.name}(${component.baseType})`, component, value[index], accum[name])
+    })
+    return;
+  }
+
+  accum[name] = decodeValue(value)
 }
-  
-  /**
-   *
-   * @param {string} abi contract abi 
-   * @param {string} calldata encoded calldata
-   * @returns {Object|null} decoded calldata
-   */
-  export function decodeCalldata(abi, calldata) {
-    try {
-      const iface = new utils.Interface(abi)
-      const sigHash = calldata.substring(0,10)
-      const decodedData = iface.decodeFunctionData(sigHash, calldata)
-      const functionABI = iface.getFunction(sigHash)
-        
-      const onlyNamedResult = decodedData.reduce((accum, value, index) => {
-        const input = functionABI.inputs[index];
 
-        decodeInput(input, value, accum)
-
-        return accum;
-      }, { });
-
-      console.log(decodedData, ' decoded data')
-      console.log(onlyNamedResult, 'only named result')
-      return onlyNamedResult;
-
-    } catch(err) {
-        
-    }
+/**
+ *
+ * @param {string} abi contract abi 
+ * @param {string} calldata encoded calldata
+ * @returns {Object|null} decoded calldata
+ */
+export function decodeCalldata(abi, calldata) {
+  try {
+    const iface = new utils.Interface(abi)
+    const sigHash = calldata.substring(0,10)
+    const decodedData = iface.decodeFunctionData(sigHash, calldata)
+    const functionABI = iface.getFunction(sigHash)
     
-    return null;
+    const onlyNamedResult = decodedData.reduce((accum, value, index) => {
+      const input = functionABI.inputs[index];
+
+      decodeInput(`Argument #${index + 1} (${input.baseType})`, input, value, accum)
+
+      return accum;
+    }, { });
+
+    return onlyNamedResult;
+  } catch(err) {
+      console.log(err, 'err')
+  }
+  return null;
 }
-  
