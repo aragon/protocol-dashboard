3fb8ee29f4390410a3d6c2960d6a51fd12595aef
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2021-05-28 22:05:23 +0400

    update ipfs check

--- src/lib/ipfs-utils.js
+++ src/lib/ipfs-utils.js
@@ -22,32 +22,43 @@ export const ipfsGet = async cid => {
 }
 
 export const getIpfsCidFromUri = string => {
-  const cidString = string.replace(/^ipfs:/, '')
-
+  const cidString = string.replace(/^ipfs:/, '');
   // if cidString can be passed to CID class without throwing
   // it means it's the actual cid
   try {
     // eslint-disable-next-line no-new
-    new CID(cidString)
-    return cidString
-  }catch(err) {}
+    new CID(cidString);
+    return cidString;
+  } catch (err) {}
 
-  // if cidString is hex from the cid's raw bytes.
+  // if cidString is ipfs v1 version hex from the cid's raw bytes and
   // we add `f` as a multibase prefix and remove `0x`
   try {
-    const cid = `f${cidString.substring(2)}`
+    const cid = `f${cidString.substring(2)}`;
     // eslint-disable-next-line no-new
-    new CID(cid)
-    return cid
-  }catch(err) {}
+    new CID(cid);
+    return cid;
+  } catch (err) {}
 
-  // if cidString is hex received from string-to-hex converter 
+  // if cidString is ipfs v0 version hex from the cid's raw bytes,
+  // we add:
+  // 1. 112 (0x70 in hex) which is dag-pb format.
+  // 2. 01 because we want to use v1 version
+  // 3. f since cidString is already hex, we only add `f` without converting anything.
   try {
-    const cid = toUTF8String(cidString)
+    const cid = `f0170${cidString.substring(2)}`;
     // eslint-disable-next-line no-new
-    new CID(cid)
-    return cid
-  }catch(err) {}
+    new CID(cid);
+    return cid;
+  } catch (err) {}
+
+  // if cidString is hex received from string-to-hex converter
+  try {
+    const cid = toUTF8String(cidString) || cidString;
+    // eslint-disable-next-line no-new
+    new CID(cid);
+    return cid;
+  } catch (err) {}
 
   return ''
 }
