5a8b1f47991faffdbc2ebf1318d5082904d0f580
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2021-06-10 20:34:50 +0400

    sentry commented and fixed

--- src/sentry.js
+++ src/sentry.js
@@ -1,21 +1,21 @@
-import { init as initSentry, captureMessage } from '@sentry/browser'
+// import { init as initSentry, captureMessage } from '@sentry/browser'
 import env from './environment'
-import { getNetworkType } from './lib/web3-utils'
+// import { getNetworkType } from './lib/web3-utils'
 
 export const sentryEnabled = Boolean(env('SENTRY_DSN'))
 
 export default function initializeSentry() {
-  if (sentryEnabled) {
-    initSentry({
-      dsn: env('SENTRY_DSN'),
-      environment: getNetworkType(env('CHAIN_ID')),
-      release: 'court-dashboard@' + env('BUILD'),
-    })
-  }
+  // if (sentryEnabled) {
+  //   initSentry({
+  //     dsn: env('SENTRY_DSN'),
+  //     environment: getNetworkType(env('CHAIN_ID')),
+  //     release: 'court-dashboard@' + env('BUILD'),
+  //   })
+  // }
 }
 
 export function logWithSentry(message, level = 'warning') {
-  if (sentryEnabled) {
-    captureMessage(message, level)
-  }
+  // if (sentryEnabled) {
+  //   captureMessage(message, level)
+  // }
 }
