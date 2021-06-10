d75a81c790e34d419f3821becee37b778af3f883
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2021-06-10 14:21:01 +0400

    comment sentry

--- src/sentry.js
+++ src/sentry.js
@@ -5,17 +5,17 @@ import { getNetworkType } from './lib/web3-utils'
 export const sentryEnabled = Boolean(env('SENTRY_DSN'))
 
 export default function initializeSentry() {
-  if (sentryEnabled) {
-    initSentry({
-      dsn: env('SENTRY_DSN'),
-      environment: getNetworkType(env('CHAIN_ID')),
-      release: 'court-dashboard@' + env('BUILD'),
-    })
-  }
+  // if (sentryEnabled) {
+  //   initSentry({
+  //     dsn: env('SENTRY_DSN'),
+  //     environment: getNetworkType(env('CHAIN_ID')),
+  //     release: 'court-dashboard@' + env('BUILD'),
+  //   })
+  // }
 }
 
 export function logWithSentry(message, level = 'warning') {
-  if (sentryEnabled) {
-    captureMessage(message, level)
-  }
+  // if (sentryEnabled) {
+  //   captureMessage(message, level)
+  // }
 }
