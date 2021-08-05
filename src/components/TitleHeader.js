61d42a54dc3bd970c0af6d582d1b0d7bb64ce9c3
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2021-08-05 16:59:02 +0400

    update links

--- src/components/TitleHeader.js
+++ src/components/TitleHeader.js
@@ -19,7 +19,7 @@ function TitleHeader({ title, onlyTitle = false }) {
             {!compactMode && (
               <Button
                 label="User guide"
-                href="https://help.aragon.org/article/41-aragon-court"
+                href="https://help.aragon.org/article/43-dispute-lifecycle"
                 css={`
                   margin-right: ${1.5 * GU}px;
                   width: 150px;
