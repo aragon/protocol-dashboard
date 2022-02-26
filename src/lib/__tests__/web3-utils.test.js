afd505f07701fc85c7ee51f83454f8cce0a6af8c
Felipe Novaes Rocha <windholyghost@gmail.com>
2022-02-25 21:49:08 -0300

    fix: check if null and threat like undefined, added jest files

---
+++ src/lib/__tests__/web3-utils.test.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+import { getDefaultProvider } from '../web3-utils'
+import { getPreferredChain } from '../../local-settings'
+
+describe('Test getDefaultProvider()', () => {
+  it('should test getDefaultProvider(null) and return default XDAI url connection', () => {
+    const ret = getDefaultProvider(null)
+    expect(ret.connection.url).not.toBe('http://localhost:8545')
+    expect(ret.connection.url).toBe('https://rpc.xdaichain.com')
+  })
+
+  it('should test getDefaultProvider() and return default XDAI url connection', () => {
+    const ret = getDefaultProvider(undefined)
+    expect(ret.connection.url).toBe('https://rpc.xdaichain.com')
+  })
+
+  it('should test getDefaultProvider(4) and return same chainID', () => {
+    const ret = getDefaultProvider(4)
+    expect(ret._network.chainId).toBe(4)
+  })
+
+  it('should test getDefaultProvider(100) and return url connection', () => {
+    const ret = getDefaultProvider(100)
+    expect(ret.connection.url).toBe('https://rpc.xdaichain.com')
+  })
+})
+
+describe('Test getPreferredChain()', () => {
+  it('should test getPreferredChain() and return 100', () => {
+    const chainID = getPreferredChain()
+    expect(chainID).toBe(100)
+  })
+})
