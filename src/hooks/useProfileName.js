7d3d98fa5de52b5264cc10ceeeb3d5d0bc438cf7
Felipe Novaes Rocha <windholyghost@gmail.com>
2022-03-23 21:39:21 -0300

    fix: remove 3box

--- src/hooks/useProfileName.js
+++ src/hooks/useProfileName.js
@@ -1,30 +1,7 @@
-import { useEffect, useState } from 'react'
-import { getProfileForAccount } from '../lib/profile'
-
-const addressCache = new Map()
+import { useState } from 'react'
 
 export default function useProfileName(account) {
-  const [profileName, setProfileName] = useState(null)
-
-  useEffect(() => {
-    let cancelled = false
-    async function fetchProfile() {
-      if (addressCache.get(account)) {
-        setProfileName(addressCache.get(account))
-        return
-      }
-      const profile = await getProfileForAccount(account)
-      if (profile && !cancelled) {
-        setProfileName(profile.name)
-        addressCache.set(account, profile.name)
-      }
-    }
-
-    fetchProfile()
-    return () => {
-      cancelled = true
-    }
-  }, [account])
-
+  const [profileName] = useState(null)
+  // 3box removed, deprecated libs
   return profileName
 }
