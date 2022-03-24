1d50dcf0d1323864dc63a08bb1a87fcfd9cb00a3
Felipe Novaes Rocha <windholyghost@gmail.com>
2022-03-24 16:36:48 -0300

    fix: disable webpack analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -2,17 +2,9 @@
 /* config-overrides.js */
 
 const { useBabelRc, override, useEslintRc } = require('customize-cra')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin
-
-const useBundleAnalyser = config => {
-  config.plugins.push(new BundleAnalyzerPlugin())
-  return config
-}
 
 module.exports = override(
   useBabelRc(),
   // eslint-disable-next-line no-path-concat
-  useEslintRc(__dirname + '/.eslintrc'),
-  useBundleAnalyser
+  useEslintRc(__dirname + '/.eslintrc')
 )
